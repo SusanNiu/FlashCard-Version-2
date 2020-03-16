b17ab8ae6d2007fb90ade9263b2e616485317c6f
YuanyuanNiu2019 <50037916+YuanyuanNiu2019@users.noreply.github.com>
2020-03-15 21:47:48 -0400

    Lab3 AddCardActivity

---
+++ app/src/main/java/com/example/flashcard2020/AddCardActivity.java
@@ -0,0 +1,24 @@
+package com.example.flashcard2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class addCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.cancelCard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+                AddCardActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
