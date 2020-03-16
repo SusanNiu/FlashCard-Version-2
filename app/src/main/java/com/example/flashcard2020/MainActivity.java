d65b6a62167835c84e6d5128e330ec76d00f6523
YuanyuanNiu2019 <50037916+YuanyuanNiu2019@users.noreply.github.com>
2020-03-16 03:28:22 -0400

    User should be able to create multiple cards and browse through their deck of created cards

--- app/src/main/java/com/example/flashcard2020/MainActivity.java
+++ app/src/main/java/com/example/flashcard2020/MainActivity.java
@@ -1,61 +1,89 @@
 package com.example.flashcard2020;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Inside of OnCreate Database, we access all Flashcards from the Database.
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+        // Check to see if the database is empty.
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-
             }
-
         });
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view){
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
-
+                Log.d("Click", "Flashcard Answer Clicked.");
             }
-
         });
         findViewById(R.id.addCard).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, addCardActivity.class);
+//              MainActivity.this.startActivityForResult(intent,100);
                 startActivityForResult(intent,100);
                 Log.d("OnClickListener", "User has clicked on addCard.");
             }
-
-            });
-
-}
-
+        });
+        findViewById(R.id.nextCardButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+                if(findViewById(R.id.flashcard_question).getVisibility() != View.VISIBLE){
+                    findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                    Log.d("Visibility", "Flashcard was facing answer when next was clicked. For UX purposes, cardfacing has been switched to question.");
+                }
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
+    }
+    // Database Outside OnCreate and List of Flashcards.
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    // Access CurrentCardDisplayedIndex outside the onCreate() Method
+    int currentCardDisplayedIndex = 0;
+    //    C:\Users\"Gian De"\AppData\Local\Android\Sdk\platform-tools\adb forward tcp:8080 tcp:8080
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // Added resultCode == RESULT_OK
         if (requestCode == 100 && resultCode == RESULT_OK) {
-            String newQuestion=data.getExtras().getString("question");
-            String newAnswer=data.getExtras().getString("answer");
-            ((TextView) findViewById(R.id.flashcard_question)).setText(newQuestion);
-            ((TextView) findViewById(R.id.flashcard_answer)).setText(newAnswer);
+            String newS1 = data.getExtras().getString("question");
+            String newS2 = data.getExtras().getString("answer");
+            ((TextView) findViewById(R.id.flashcard_question)).setText(newS1);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(newS2);
+            // Insert Card into Database after saving.
+            flashcardDatabase.insertCard(new Flashcard(newS1, newS2));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
-
-
-
 }
--- app/src/main/java/com/example/flashcard2020/addCardActivity.java
+++ app/src/main/java/com/example/flashcard2020/addCardActivity.java
@@ -1,24 +1,21 @@
 package com.example.flashcard2020;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class addCardActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
-
         findViewById(R.id.cancelCard).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            finish();
+            @Override
+            public void onClick(View v) {
+//            Intent intent = new Intent(addCardActivity.this, MainActivity.class);
+//            addCardActivity.this.startActivity(intent);
+                finish();
             }
         });
         findViewById(R.id.saveBtn).setOnClickListener(new View.OnClickListener() {
@@ -27,13 +24,13 @@ public class addCardActivity extends AppCompatActivity {
                 String somestring =((EditText)findViewById(R.id.newQuestion)).getText().toString();
                 String anotherstring = ((EditText)findViewById(R.id.newAnswer)).getText().toString();
                 Intent data = new Intent();
+//                data.putExtra("string1", somestring);
+//                data.putExtra("string2", anotherstring);
                 data.putExtra("question", somestring);
                 data.putExtra("answer", anotherstring);
                 setResult(RESULT_OK, data);
                 finish();
-
             }
         });
-
     }
 }
